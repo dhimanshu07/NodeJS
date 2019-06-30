65b734df1b81ea2be27dd706a01864440a159fbf
Himanshu deswal <36129430+dhimanshu07@users.noreply.github.com>
2019-06-30 02:14:01 +0100

    Node JS   and MYSQL
    Nodejs frame work connected to mysql and used express for front end

--- index.js
+++ index.js
@@ -1,45 +1,102 @@
-const express = require('express');
-const Datastore = require('nedb');
+const mysql = require('mysql');
+const express  = require('express');
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.listen(3000, ()=> console.log('listening to 3000'));
 app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
 
-const database = new Datastore('database.db');
-database.loadDatabase();
 
-app.post('/api', (request, response) => {
-  const data = request.body;
-  console.log(request.body);	
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  console.log(database);
-  response.json(data);
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'client'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.get('/site_power', (request, response) => {
+con.query("SELECT * FROM site_power", (error, rows, fields) =>{
+		if(error)throw error;
+			console.log("Successfully ");
+			response.send(rows);
+			console.log(rows);
+			});
 });
 
 
- 
-//database.insert({name: 'Himanshu', status: 'ok'});
-//database.insert({name: 'Toshi', status:'good'});
-app.get('/api', (request, response) =>{
-	database.find({}, (err, data) => {
-		if(err){
-			response.end(); 
-			return;
-		} 
-       response.json(data);
+
+
+app.get('/site_power/:id', (request,response)=>{
+
+
+	con.query("SELECT * FROM site_power WHERE id = ?", [request.params.id], (err, rows, fields)=>{
+
+		if(err) throw err;
+		response.send(rows);
+		console.log(rows);
+
+		console.log("here is your data");
 	});
 });
 
 
 
-/*const mymap = L.map('mymap').setView([lat, lon], 15);
-          const attribution =
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-          const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-          const tiles = L.tileLayer(tileUrl, { attribution });
-          tiles.addTo(mymap);
-          const marker = L.marker([lat, lon]).addTo(mymap);
-        });*/
+app.get('/',(request,response)=>{
+response.sendfile('Dsu/index.html');
+});
+
+
+
+app.post('/insert', (request, response)=>{
+
+
+	const data = request.body.var2;
+	console.log(request.body);
+
+ con.query("INSERT INTO dsus (description) VALUES ('"+request.body.var2+"')", (error, rows, fields)=>{
+  
+  if(error) throw error;
+  response.send(rows);
+  console.log("Inserted Successfully");
+ });
+
+});
+
+/* USE 'employeedb';
+DROP procedure IF EXISTS 'abcd';
+
+DELIMITER $$
+USE 'employeedb' $$
+
+CREATE PROCEDURE 'abcd'(
+	IN _EmpID INT,
+	IN _Name varchar(45),
+	IN _EmpCode varchar(45),
+	IN _salary INT
+	)
+
+BEGIN
+    IF _EmpID = 0 THEN
+    INSERT INTO employee(Name,Empcode,Salary)
+    VALUES (_Name,_EmpCode,_Salary);
+
+    SET _EmpID = LAST_INSERT_ID();
+
+    ELSE
+      UPDATE Employee
+      SET
+      Name = _Name,
+      EmpCode = _EmpCode,
+      Salary = _Salary
+      WHERE EmpID = _EmpID;
+      END IF;
+
+      SELECT _EmpID AS 'EmpID';
+
+      END */
---
+++ public/Dsu/sketch.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('submit');
+button.addEventListener('click', async event =>{
+
+	const var2 = document.getElementById('description').value;
+
+	
+	const data = {var2};
+
+});
